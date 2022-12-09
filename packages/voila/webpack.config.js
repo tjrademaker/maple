c679dbb2508b2a12a4541f4bcf740cd4db256201
Thomas Rademaker <thomas.rademaker@mail.mcgill.ca>
2022-12-09 15:04:47 +1100

    added voila setup

---
+++ packages/voila/webpack.config.js
@@ -0,0 +1,83 @@
+// Copyright (c) Jupyter Development Team.
+// Copyright (c) Voila Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+const fs = require('fs-extra');
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge').default;
+const { ModuleFederationPlugin } = webpack.container;
+
+const Build = require('@jupyterlab/builder').Build;
+const baseConfig = require('@jupyterlab/builder/lib/webpack.config.base');
+
+const data = require('./package.json');
+
+const names = Object.keys(data.dependencies).filter(name => {
+  const packageData = require(path.join(name, 'package.json'));
+  return packageData.jupyterlab !== undefined;
+});
+
+// Ensure a clear build directory.
+const buildDir = path.resolve(__dirname, 'build');
+if (fs.existsSync(buildDir)) {
+  fs.removeSync(buildDir);
+}
+fs.ensureDirSync(buildDir);
+
+// Copy files to the build directory
+const libDir = path.resolve(__dirname, 'lib');
+const style = path.resolve(__dirname, 'style.css');
+fs.copySync(libDir, buildDir);
+fs.copySync(style, path.resolve(buildDir, 'style.css'));
+
+const extras = Build.ensureAssets({
+  packageNames: names,
+  output: buildDir
+});
+
+// Make a bootstrap entrypoint
+const entryPoint = path.join(buildDir, 'bootstrap.js');
+
+if (process.env.NODE_ENV === 'production') {
+  baseConfig.mode = 'production';
+}
+
+const distRoot = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'share',
+  'jupyter',
+  'voila',
+  'templates',
+  'base',
+  'static'
+);
+
+module.exports = [
+  merge(baseConfig, {
+    mode: 'development',
+    entry: ['./publicpath.js', './' + path.relative(__dirname, entryPoint)],
+    output: {
+      path: distRoot,
+      library: {
+        type: 'var',
+        name: ['_JUPYTERLAB', 'CORE_OUTPUT']
+      },
+      filename: 'voila.js'
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        library: {
+          type: 'var',
+          name: ['_JUPYTERLAB', 'CORE_LIBRARY_FEDERATION']
+        },
+        name: 'CORE_FEDERATION',
+        shared: {
+          ...data.dependencies
+        }
+      })
+    ]
+  })
+].concat(extras);
